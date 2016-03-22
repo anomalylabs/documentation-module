84504be7685daec2efe6001b1b62f0339cd02971
Ryan Thompson <service@aiwebsystems.com>
2016-03-22 02:09:59 -0500

    Source

---
+++ src/Source/SourceExtension.php
@@ -0,0 +1,16 @@
+<?php namespace Anomaly\DocumentationModule\Source;
+
+use Anomaly\Streams\Platform\Addon\Extension\Extension;
+
+/**
+ * Class SourceExtension
+ *
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
+ * @package       Anomaly\DocumentationModule\Source
+ */
+class SourceExtension extends Extension
+{
+
+}
