2a5a71576c5e0949d350d2bd60803ba3bbccb2d6
Ryan Thompson <service@aiwebsystems.com>
2016-09-18 19:43:15 -0500

    Pags

--- src/Project/ProjectCollection.php
+++ src/Project/ProjectCollection.php
@@ -1,8 +1,31 @@
 <?php namespace Anomaly\DocumentationModule\Project;
 
+use Anomaly\DocumentationModule\Project\Contract\ProjectInterface;
 use Anomaly\Streams\Platform\Entry\EntryCollection;
 
+/**
+ * Class ProjectCollection
+ *
+ * @link          http://pyrocms.com/
+ * @author        PyroCMS, Inc. <support@pyrocms.com>
+ * @author        Ryan Thompson <ryan@pyrocms.com>
+ */
 class ProjectCollection extends EntryCollection
 {
 
+    /**
+     * Return only enabled projects.
+     *
+     * @return ProjectCollection
+     */
+    public function enabled()
+    {
+        return $this->filter(
+            function ($project) {
+
+                /* @var ProjectInterface $project */
+                return $project->isEnabled();
+            }
+        );
+    }
 }
