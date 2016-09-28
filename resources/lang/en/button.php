9682da01df435711fede2bfb1ea4b31c7f2b8e39
Ryan Thompson <service@aiwebsystems.com>
2016-09-28 01:42:27 -0500

    Lang

--- resources/lang/en/button.php
+++ resources/lang/en/button.php
@@ -2,10 +2,11 @@
 
 return [
     'sections'             => 'Sections',
-    'versions'          => 'Versions',
-    'new_type'          => 'New Type',
+    'versions'             => 'Versions',
+    'new_type'             => 'New Type',
     'new_section'          => 'New Section',
-    'new_project'       => 'New Project',
-    'new_version'       => 'New Version',
+    'new_project'          => 'New Project',
+    'new_version'          => 'New Version',
+    'new_category'         => 'New Category',
     'create_child_section' => 'Create Child Section',
 ];
