7368c39ed466bb5ad0524d7e3bbe6a568f99efc5
Ryan Thompson <service@aiwebsystems.com>
2017-11-08 07:40:33 -0600

    We don't need these gadgets.

--- resources/js/initialize.js
+++ resources/js/initialize.js
@@ -1,48 +1,6 @@
 $(function () {
 
     /**
-     * Add copy to clipboard icons
-     * to all the code blocks.
-     */
-    $('code').each(function () {
-        $(this).after('<span class="copy-to-clipboard" title="Copied to clipboard!" />');
-    });
-
-    /**
-     * Handle the clipboard actions.
-     * @type {Clipboard}
-     */
-    let clipboard = new Clipboard('.copy-to-clipboard', {
-        text: function (trigger) {
-            return $(trigger).prev('code').text().replace(/^\$\s/gm, '');
-        }
-    });
-
-    /**
-     * Show tooltips when the clipboard
-     * succeeds in copying to clipboard.
-     */
-    clipboard.on('success', function (e) {
-
-        $(e.trigger)
-            .attr('data-placement', 'left')
-            .tooltip('enable')
-            .tooltip('show');
-
-        $(e.trigger).on('mouseleave', function () {
-            $(this)
-                .tooltip('hide')
-                .tooltip('disable')
-        });
-
-        setTimeout(function () {
-            $(e.trigger)
-                .tooltip('hide')
-                .tooltip('disable');
-        }, 1000);
-    });
-
-    /**
      * Add anchors for deep nesting in documentation.
      * @type {{visible: string, placement: string, truncate: number}}
      */
