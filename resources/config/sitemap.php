580280b162dfcddb9afe4fcbc4fc045450c24e26
Ryan Thompson <ryan@pyrocms.com>
2019-03-03 17:17:53 -0600

    Update sitemap n shit

--- resources/config/sitemap.php
+++ resources/config/sitemap.php
@@ -1,28 +1,6 @@
 <?php
 
-use Anomaly\DocumentationModule\Project\Contract\ProjectInterface;
-use Anomaly\DocumentationModule\Project\Contract\ProjectRepositoryInterface;
-use Roumen\Sitemap\Sitemap;
-
 return [
-    'lastmod' => function (ProjectRepositoryInterface $projects) {
-
-        if (!$project = $projects->lastModified()) {
-            return null;
-        }
-
-        return $project->lastModified()->toAtomString();
-    },
-    'entries' => function (ProjectRepositoryInterface $projects) {
-        return $projects->all();
-    },
-    'handler' => function (Sitemap $sitemap, ProjectInterface $entry) {
-
-        $sitemap->add(
-            url($entry->route('view')),
-            $entry->lastModified()->toAtomString(),
-            0.5,
-            'monthly'
-        );
-    },
+    'projects' => \Anomaly\DocumentationModule\Project\Contract\ProjectRepositoryInterface::class,
+    'pages'    => \Anomaly\DocumentationModule\Page\Contract\PageRepositoryInterface::class,
 ];
